7ec9b86c6cf0905e72e8d890ba7e4f96aaa6c061
UdayKiranLues <uday998588@gmail.com>
2025-08-01 11:53:16 +0530

    BaseURL changed

--- src/configure.jsx
+++ src/configure.jsx
@@ -1,5 +1,5 @@
 
-export const baseurl = "http://64.227.131.13:5000"
+export const baseurl = "http://64.227.131.13:5001"
 export const authentication = {
     login_url: `${baseurl}/api/v1/auth/login`,
     cart_url: `${baseurl}/api/v1/cart/list`,
